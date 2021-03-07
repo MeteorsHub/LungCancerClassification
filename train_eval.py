116c30bb92f130dce87ecc92347370c10fff47c2
MeteorsHub <jimranor@outlook.com>
2021-03-07 18:43:35 +0800

    add voting model

--- train_eval.py
+++ train_eval.py
@@ -4,17 +4,11 @@ import os
 import pickle
 
 import matplotlib.pyplot as plt
-import numpy as np
-from sklearn import svm, metrics, naive_bayes
-from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier, GradientBoostingClassifier
+from sklearn import metrics, base
 from sklearn.model_selection import GridSearchCV
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.neural_network import MLPClassifier
-from sklearn.preprocessing import label_binarize
-from sklearn.tree import DecisionTreeClassifier
 
 from data_loader import MarkerExpressionDataset
-from utils import load_ymal, save_yaml, maybe_create_path, double_print
+from utils import load_ymal, save_yaml, maybe_create_path, double_print, eval_results, get_model
 from visualization import plot_table, plot_feature_distribution
 
 parser = argparse.ArgumentParser()
@@ -27,82 +21,6 @@ parser.add_argument('-r', '--retrain', action='store_true',
 parser.add_argument('-o', '--overwrite_config', action='store_true',
                     help='if set, ignore existing config file in exp path and use that in config folder')
 args = parser.parse_args()
-
-
-def evaluate(y_true, y_score, labels, average='macro', threshold=0.5, num_fold=None):
-    """
-
-    :param y_true: [n_samples]
-    :param y_score: [n_samples, n_classes]
-    :param labels: the y_score order of y_true label
-    :param average:
-    :param threshold: only used in binary classification
-    :param num_fold:
-    :return:
-    """
-    y_true = np.array(y_true)
-    y_score = np.array(y_score)
-    all_results = dict()
-    # threshold average
-    if len(labels) == 2:
-        binary = True
-    else:
-        binary = False
-
-    roc_curves = []
-    roc_auc_scores = []
-    y_true_binary_all = label_binarize(y_true, classes=labels)
-    for i, class_i in enumerate(labels):
-        y_true_binary = y_true_binary_all[:, i]
-        y_score_binary = y_score[:, i]
-        roc_curves.append(metrics.roc_curve(y_true_binary, y_score_binary))
-        roc_auc_scores.append(100 * metrics.roc_auc_score(y_true_binary, y_score_binary))
-    all_results['roc_curve'] = roc_curves
-    all_results['roc_auc_score'] = roc_auc_scores
-    all_results['roc_auc_score_avg'] = 100 * metrics.roc_auc_score(
-        y_true, y_score, average=average, multi_class='ovr', labels=labels)
-
-    # threshold specific
-    if binary:
-        y_pred = np.where(y_score[:, -1] > threshold,
-                          labels[-1] * np.ones_like(y_true),
-                          labels[0] * np.ones_like(y_true))
-    else:
-        y_pred = np.argmax(y_score, 1)
-        for sample_i in range(len(y_pred)):
-            y_pred[sample_i] = labels[y_pred[sample_i]]
-    all_results['conf_mat'] = metrics.confusion_matrix(y_true, y_pred, labels=labels)
-
-    tps = np.diagonal(all_results['conf_mat'])
-    ps = np.sum(all_results['conf_mat'], 1)
-    sensitivity = 100 * tps / ps
-    specificity = np.zeros_like(sensitivity)
-    for i in range(len(specificity)):
-        all_negative_tps = 0
-        all_negative_ps = 0
-        for j in range(len(specificity)):
-            if i != j:
-                all_negative_tps += tps[j]
-                all_negative_ps += ps[j]
-        specificity[i] = all_negative_tps / all_negative_ps
-    specificity *= 100
-    all_results['sensitivity'] = sensitivity
-    all_results['specificity'] = specificity
-    all_results['f1_avg'] = 100 * metrics.f1_score(y_true, y_pred, average=average)
-    all_results['f1'] = 100 * metrics.f1_score(y_true, y_pred, average=None)
-
-    if num_fold is not None:
-        all_fold_roc_auc_scores = []
-        all_fold_f1s = []
-        for fold_i in range(num_fold):
-            start_i = fold_i * len(y_true) // num_fold
-            end_i = (fold_i + 1) * len(y_true) // num_fold
-            all_fold_f1s.append(100 * metrics.f1_score(y_true[start_i:end_i], y_pred[start_i:end_i], average=average))
-            all_fold_roc_auc_scores.append(100 * metrics.roc_auc_score(
-                y_true[start_i:end_i], y_score[start_i:end_i], average=average, multi_class='ovr', labels=labels))
-        all_results['f1_avg_std'] = np.std(all_fold_f1s)
-        all_results['roc_auc_score_avg_std'] = np.std(all_fold_roc_auc_scores)
-    return all_results
 
 
 def train_eval(config, exp_path):
@@ -129,30 +47,11 @@ def train_eval(config, exp_path):
                     f.write('%s:\t%s\n' % (features[feature_index], flag))
                     feature_index = (feature_index + 1) % len(features)
 
-    if config['model'] == 'svm':
-        model_class = svm.SVC
-    elif config['model'] == 'rdf':
-        model_class = RandomForestClassifier
-    elif config['model'] == 'adaboost':
-        model_class = AdaBoostClassifier
-    elif config['model'] == 'gradient_boost':
-        model_class = GradientBoostingClassifier
-    elif config['model'] == 'gaussion_bayes':
-        model_class = naive_bayes.GaussianNB
-    elif config['model'] == 'mlp':
-        model_class = MLPClassifier
-    elif config['model'] == 'k_neighbors':
-        model_class = KNeighborsClassifier
-    elif config['model'] == 'decision_tree':
-        model_class = DecisionTreeClassifier
-    else:
-        raise AttributeError('unrecognized model %s' % config['model'])
-
-    threshold = config.get('threshold', 0.5)
+    threshold = config.get('threshold', 'roc_optimal')
     metrics_names = ['sensitivity', 'specificity', 'f1', 'roc_auc_score']
     metrics_avg_names = ['f1_avg', 'f1_avg_std', 'roc_auc_score_avg', 'roc_auc_score_avg_std']
 
-    fig, ax = plt.subplots(8, len(dataset.markers), squeeze=False, figsize=(6 * len(dataset.markers), 30))
+    fig, ax = plt.subplots(9, len(dataset.markers), squeeze=False, figsize=(6 * len(dataset.markers), 40))
     metrics_file = open(os.path.join(exp_path, 'metrics.txt'), 'w')
     metrics_fig_filename = os.path.join(exp_path, 'conf_mat.png')
     best_params = dict()
@@ -162,7 +61,8 @@ def train_eval(config, exp_path):
         # parameter search
         print('parameter search for marker %s...' % marker)
         all_x, all_y, cv_index = dataset.get_all_data(marker)
-        best_model = GridSearchCV(model_class(),
+        model = get_model(config)
+        best_model = GridSearchCV(model,
                                   param_grid=config['model_kwargs'],
                                   cv=cv_index,
                                   scoring='roc_auc_ovr')
@@ -178,7 +78,7 @@ def train_eval(config, exp_path):
         test_ys = []
         test_ys_score = []
         for fold_i, (train_x, train_y, test_x, test_y) in enumerate(dataset.get_split_data(marker)):
-            model = model_class()
+            model = base.clone(model)
             model.set_params(**best_params[marker])
             model.classes_ = dataset.classes
             model.fit(train_x, train_y)
@@ -196,12 +96,12 @@ def train_eval(config, exp_path):
             with open(model_filename, 'wb') as f:
                 pickle.dump(model, f)
 
-        train_metrics = evaluate(train_ys, train_ys_score,
-                                 labels=dataset.classes, average='macro',
-                                 threshold=threshold, num_fold=dataset.num_fold)
-        test_metrics = evaluate(test_ys, test_ys_score,
-                                labels=dataset.classes, average='macro',
-                                threshold=threshold, num_fold=dataset.num_fold)
+        train_metrics = eval_results(train_ys, train_ys_score,
+                                     labels=dataset.classes, average='macro',
+                                     threshold=threshold, num_fold=dataset.num_fold)
+        test_metrics = eval_results(test_ys, test_ys_score,
+                                    labels=dataset.classes, average='macro',
+                                    threshold=train_metrics['used_threshold'], num_fold=dataset.num_fold)
         all_marker_train_metrics.append(train_metrics)
         all_marker_test_metrics.append(test_metrics)
 
@@ -209,7 +109,7 @@ def train_eval(config, exp_path):
         double_print('marker: %s' % marker, metrics_file)
         double_print('metrics on training set:', metrics_file)
         for j, class_j in enumerate(dataset.classes):
-            log_str = '[class: %s] ' % class_j
+            log_str = '[class: %s. threshold: %1.1f] ' % (class_j, 100 * train_metrics['used_threshold'][j])
             for metrics_name in metrics_names:
                 log_str += '%s: %1.1f. ' % (metrics_name, train_metrics[metrics_name][j])
             double_print(log_str, metrics_file)
@@ -217,7 +117,7 @@ def train_eval(config, exp_path):
             double_print('%s: %1.1f' % (metrics_name, train_metrics[metrics_name]), metrics_file)
         double_print('metrics on test set:', metrics_file)
         for j, class_j in enumerate(dataset.classes):
-            log_str = '[class: %s] ' % class_j
+            log_str = '[class: %s. threshold: %1.1f] ' % (class_j, 100 * test_metrics['used_threshold'][j])
             for metrics_name in metrics_names:
                 log_str += '%s: %1.1f. ' % (metrics_name, test_metrics[metrics_name][j])
             double_print(log_str, metrics_file)
@@ -252,12 +152,19 @@ def train_eval(config, exp_path):
         current_ax.set_title('%s on train set of all folds' % marker)
 
         current_ax = ax[3, i]
-        for roc_curve, roc_auc_score in zip(train_metrics['roc_curve'], train_metrics['roc_auc_score']):
-            metrics.RocCurveDisplay(fpr=roc_curve[0], tpr=roc_curve[1], roc_auc=roc_auc_score).plot(ax=current_ax)
+        for j in range(len(dataset.classes)):
+            roc_curve = train_metrics['roc_curve'][j]
+            roc_auc_score = train_metrics['roc_auc_score'][j]
+            class_id = dataset.classes[j]
+            sen = train_metrics['sensitivity'][j] / 100
+            spe = train_metrics['specificity'][j] / 100
+            metrics.RocCurveDisplay(fpr=roc_curve[0], tpr=roc_curve[1],
+                                    roc_auc=roc_auc_score, estimator_name='class %s' % class_id).plot(ax=current_ax)
+            current_ax.scatter(1 - spe, sen)
 
         current_ax = ax[4, i]
-        table_val_list = [dataset.classes]
-        row_labels = ['cls']
+        table_val_list = [dataset.classes, [100 * item for item in train_metrics['used_threshold']]]
+        row_labels = ['cls', 'thr']
         for metrics_name in metrics_names:
             table_val_list.append(train_metrics[metrics_name])
             row_labels.append(metrics_name[:min(3, len(metrics_name))])
@@ -283,8 +190,19 @@ def train_eval(config, exp_path):
         current_ax.set_title('%s on test set of all folds' % marker)
 
         current_ax = ax[7, i]
-        table_val_list = [dataset.classes]
-        row_labels = ['cls']
+        for j in range(len(dataset.classes)):
+            roc_curve = test_metrics['roc_curve'][j]
+            roc_auc_score = test_metrics['roc_auc_score'][j]
+            class_id = dataset.classes[j]
+            sen = test_metrics['sensitivity'][j] / 100
+            spe = test_metrics['specificity'][j] / 100
+            metrics.RocCurveDisplay(fpr=roc_curve[0], tpr=roc_curve[1],
+                                    roc_auc=roc_auc_score, estimator_name='class %s' % class_id).plot(ax=current_ax)
+            current_ax.scatter(1 - spe, sen)
+
+        current_ax = ax[8, i]
+        table_val_list = [dataset.classes, [100 * item for item in test_metrics['used_threshold']]]
+        row_labels = ['cls', 'thr']
         for metrics_name in metrics_names:
             table_val_list.append(test_metrics[metrics_name])
             row_labels.append(metrics_name[:min(3, len(metrics_name))])
--- utils.py
+++ utils.py
@@ -3,6 +3,12 @@ import os
 
 import numpy as np
 import yaml
+from sklearn import svm, metrics, naive_bayes
+from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier, GradientBoostingClassifier, VotingClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.neural_network import MLPClassifier
+from sklearn.preprocessing import label_binarize
+from sklearn.tree import DecisionTreeClassifier
 
 
 def read_dict_csv(filename: str, return_fieldnames=False) -> (list, list):
@@ -73,3 +79,131 @@ def get_bounds(feature, method='min_max', extend_factor=0.1):
 
 def unstack(a, axis=0):
     return [np.squeeze(e, axis) for e in np.split(a, a.shape[axis], axis=axis)]
+
+
+def find_roc_optimal_cutoff_point(fprs, tprs, thresholds):
+    assert len(fprs) == len(tprs) == len(thresholds)
+    cutoff_fpr = fprs[0]
+    cutoff_tpr = tprs[0]
+    cutoff_threshold = thresholds[0]
+    objective = (1 - cutoff_fpr) + cutoff_tpr
+    for fpr_, tpr_, threshold_ in zip(fprs, tprs, thresholds):
+        if (1 - fpr_) + tpr_ > objective:
+            cutoff_fpr = fpr_
+            cutoff_tpr = tpr_
+            cutoff_threshold = threshold_
+            objective = (1 - fpr_) + tpr_
+    return cutoff_fpr, cutoff_tpr, cutoff_threshold
+
+
+def eval_results(y_true, y_score, labels, average='macro', threshold='roc_optimal', num_fold=None):
+    """
+    Note that in multi-class problems, only f1-related metrics are multi-class,
+    the others are multiple binary-class metrics.
+    :param y_true: [n_samples]
+    :param y_score: [n_samples, n_classes]
+    :param labels: the y_score order of y_true label
+    :param average:
+    :param threshold: list of float (size labels), single float or 'roc_optimal'. only used in binary-class metrics
+    :param num_fold: if not None, compute std metrics
+    :return:
+    """
+    y_true = np.array(y_true)
+    y_score = np.array(y_score)
+    if isinstance(threshold, list) or isinstance(threshold, tuple):
+        assert len(threshold) == len(labels)
+    else:
+        threshold = [threshold for _ in labels]
+    all_results = dict()
+    # threshold average
+    if len(labels) == 2:
+        binary = True
+    else:
+        binary = False
+
+    roc_curves = []
+    roc_auc_scores = []
+    used_thresholds = []
+    sensitivity = []
+    specificity = []
+    y_true_binary_all = label_binarize(y_true, classes=labels)
+    for i, class_i in enumerate(labels):
+        y_true_binary = y_true_binary_all[:, i]
+        y_score_binary = y_score[:, i]
+        roc_curve = metrics.roc_curve(y_true_binary, y_score_binary)
+        if threshold[i] == 'roc_optimal':
+            _, _, used_threshold = find_roc_optimal_cutoff_point(roc_curve[0], roc_curve[1], roc_curve[2])
+        else:
+            used_threshold = threshold[i]
+        used_thresholds.append(used_threshold)
+        roc_curves.append(roc_curve)
+        y_label_binary = (y_score_binary > used_threshold).astype(np.int32)
+        roc_auc_scores.append(100 * metrics.roc_auc_score(y_true_binary, y_score_binary))
+        sensitivity.append(100 * metrics.recall_score(y_true_binary, y_label_binary))
+        specificity.append(100 * metrics.recall_score(1 - y_true_binary, 1 - y_label_binary))
+    all_results['roc_curve'] = roc_curves
+    all_results['roc_auc_score'] = roc_auc_scores
+    all_results['used_threshold'] = used_thresholds
+    all_results['sensitivity'] = sensitivity
+    all_results['specificity'] = specificity
+    all_results['roc_auc_score_avg'] = 100 * metrics.roc_auc_score(
+        y_true, y_score, average=average, multi_class='ovr', labels=labels)
+
+    # threshold specific
+    if binary:
+        y_pred = np.where(y_score[:, -1] > used_thresholds[-1],
+                          labels[-1] * np.ones_like(y_true),
+                          labels[0] * np.ones_like(y_true))
+    else:
+        y_pred = np.argmax(y_score, 1)
+        for sample_i in range(len(y_pred)):
+            y_pred[sample_i] = labels[y_pred[sample_i]]
+    all_results['conf_mat'] = metrics.confusion_matrix(y_true, y_pred, labels=labels)
+
+    all_results['f1_avg'] = 100 * metrics.f1_score(y_true, y_pred, average=average)
+    all_results['f1'] = 100 * metrics.f1_score(y_true, y_pred, average=None)
+
+    if num_fold is not None:
+        all_fold_roc_auc_scores = []
+        all_fold_f1s = []
+        for fold_i in range(num_fold):
+            start_i = fold_i * len(y_true) // num_fold
+            end_i = (fold_i + 1) * len(y_true) // num_fold
+            all_fold_f1s.append(100 * metrics.f1_score(y_true[start_i:end_i], y_pred[start_i:end_i], average=average))
+            all_fold_roc_auc_scores.append(100 * metrics.roc_auc_score(
+                y_true[start_i:end_i], y_score[start_i:end_i], average=average, multi_class='ovr', labels=labels))
+        all_results['f1_avg_std'] = np.std(all_fold_f1s)
+        all_results['roc_auc_score_avg_std'] = np.std(all_fold_roc_auc_scores)
+    return all_results
+
+
+def get_model(config):
+    if config['model'] == 'svm':
+        model = svm.SVC()
+    elif config['model'] == 'rdf':
+        model = RandomForestClassifier()
+    elif config['model'] == 'adaboost':
+        if 'sub_model' in config:
+            base_estimator = get_model(config['sub_model'])
+            base_estimator.set_params(**config['sub_model']['model_kwargs'])
+        else:
+            base_estimator = None
+        model = AdaBoostClassifier(base_estimator=base_estimator)
+    elif config['model'] == 'gradient_boost':
+        model = GradientBoostingClassifier()
+    elif config['model'] == 'gaussion_bayes':
+        model = naive_bayes.GaussianNB()
+    elif config['model'] == 'mlp':
+        model = MLPClassifier()
+    elif config['model'] == 'k_neighbors':
+        model = KNeighborsClassifier()
+    elif config['model'] == 'decision_tree':
+        model = DecisionTreeClassifier()
+    elif config['model'] == 'voting':
+        sub_models = []
+        for sub_model in config['sub_model']:
+            sub_models.append([sub_model['model_name'], get_model(sub_model)])
+        model = VotingClassifier(sub_models)
+    else:
+        raise AttributeError('unrecognized model %s' % config['model'])
+    return model
